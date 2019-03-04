f87f96dc58211e09184e7b390f04a1edd4d53be7
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-03-04 15:46:02 +0000

    Basic Redux Setup

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({});
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
